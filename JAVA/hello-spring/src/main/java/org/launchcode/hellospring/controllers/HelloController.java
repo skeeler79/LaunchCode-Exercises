3767812e90d0588fa7fd7ff51200f08f0d1473ad
Sabrina Keeler <92762914+skeeler79@users.noreply.github.com>
2022-08-22 16:32:51 -0400

    finished exercises

--- JAVA/hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ JAVA/hello-spring/src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@ResponseBody
+@RequestMapping("hello")
 public class HelloController {
 
 //    @GetMapping("hello")
@@ -14,7 +16,7 @@ public class HelloController {
 
     @GetMapping("goodbye")
     @ResponseBody
-    public String goodbye(){
+    public String goodbye() {
         return "Goodbye, Spring!";
     }
 
@@ -22,24 +24,35 @@ public class HelloController {
 
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
     @ResponseBody
-    public String helloWithQueryParam(@RequestParam String name){
-        return "hello, " + name + "!";}
+    public String helloWithQueryParam(@RequestParam String name, @RequestParam String languages) {
+        String properGreeting = HelloController.createMessage(name, languages);
+        return "<h3 style = 'color:purple;'>" + properGreeting + "</h3>";
+    }
 
 
     //handles requests of the form /hello/LaunchCode
-    @GetMapping("hello/{name}")
+    @GetMapping("{name}")
     @ResponseBody
-    public String helloWithPathParam(@PathVariable String name){
-         return "Hello" + name + "!";
+    public String helloWithPathParam(@PathVariable String name) {
+        return "Hello" + name + "!";
     }
 
     @GetMapping("form")
-    @ResponseBody
-    public String helloForm (){
+//    @ResponseBody
+    public String helloForm() {
         return "<html>" +
                 "<body>" +
                 "<form action = 'hello' method = 'post'>" +
                 "<input type = 'text' name = 'name'>" +
+                "<label for = 'language-select'>Select a language:</label>" +
+                "<select name = 'languages' id = 'language-select'>" +
+                "<option value = 'English'>English</option>" +
+                "<option value = 'French'>French</option>" +
+                "<option value = 'Spanish'>Spanish</option>" +
+                "<option value = 'Italian'>Italian</option>" +
+                "<option value = 'Chinese'>Chinese</option>" +
+                "<option value = 'German'>German</option>" +
+                "</select>" +
                 "<input type = 'submit' value = 'Greet me!'>" +
                 "</form>" +
                 "</body>" +
@@ -47,6 +60,26 @@ public class HelloController {
 
     }
 
-
+    public static String createMessage(String name, String languages) {
+        if (languages.equals("English")) {
+            return "Hello, " + name;
+        } else if (languages.equals("French")) {
+            return "Bonjour, " + name;
+        } else if (languages.equals("Spanish")) {
+            return "Hola, " + name;
+        } else if (languages.equals("Italian")) {
+            return "Ciao, " + name;
+        } else if (languages.equals("Chinese")) {
+            return "Ni hao, " + name;
+        } else if (languages.equals("German")) {
+            return "Hallo, " + name;
+        }
+        return "Hello, " + name;
     }
+}
+
+
+
+
+
 
