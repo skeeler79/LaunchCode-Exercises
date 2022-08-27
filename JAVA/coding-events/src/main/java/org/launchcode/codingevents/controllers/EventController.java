4584a900de5c9ddfbc207d8fb24e799a92cd40d3
Sabrina Keeler <92762914+skeeler79@users.noreply.github.com>
2022-08-26 22:04:51 -0400

    finished second video

---
+++ JAVA/coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -0,0 +1,31 @@
+package org.launchcode.codingevents.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+
+    @GetMapping
+    public String eventsList(Model model){
+        List<String> events = new ArrayList<>();
+        events.add("Poppy's Birthday");
+        events.add("Pool Party");
+        events.add("Dinner");
+        model.addAttribute("events", events);
+
+        return "events/index";
+    }
+
+    //lives at /events/create
+    @GetMapping("create")
+    public String renderCreateEventForm(){
+        return "events/create";
+    }
+}
