57c9d7e46381520cc06cd3cf7c4772ecace95897
Sabrina Keeler <92762914+skeeler79@users.noreply.github.com>
2022-08-27 16:22:21 -0400

    finished video 1.3 Process Form

--- JAVA/coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ JAVA/coding-events/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,5 +1,6 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +18,7 @@ public class EventController {
     private static List<String> events = new ArrayList<>();
 
     @GetMapping
-    public String eventsList(Model model){
+    public String displayAllEvents(Model model){
 
         model.addAttribute("events", events);
 
@@ -29,6 +30,18 @@ public class EventController {
     public String renderCreateEventForm(){
         return "events/create";
     }
+
+//    @GetMapping
+//    public String displayAllEvents(Model model){
+//        model.addAttribute(("title", "All Events");
+//        model.addAttribute("events", events);
+//        return "events/index";
+//    }
+//    @GetMapping("create")
+//    public String displayCreateEventForm(Model model){
+//        model.addAttribute("title", "Create Event");
+//        return "events/create";
+//    }
 
     @PostMapping("create")
     public String createEvent(@RequestParam String eventName){
