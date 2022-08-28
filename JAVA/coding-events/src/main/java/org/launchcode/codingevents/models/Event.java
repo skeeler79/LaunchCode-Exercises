39eaf4da200bb721f86c3811348dd9e80fccdc92
Sabrina Keeler <92762914+skeeler79@users.noreply.github.com>
2022-08-28 10:48:13 -0400

    completed video 2.2 create an ID

--- JAVA/coding-events/src/main/java/org/launchcode/codingevents/models/Event.java
+++ JAVA/coding-events/src/main/java/org/launchcode/codingevents/models/Event.java
@@ -3,6 +3,9 @@ import java.util.Objects;
 
 public class Event {
 
+    private int id;
+    private static int nextId = 1;
+
     private String name;
     private String description;
 
@@ -11,6 +14,8 @@ public class Event {
     public Event(String name, String description) {
         this.name = name;
         this.description = description;
+        this.id = nextId;
+        nextId++;
     }
 
     public String getName() {
@@ -28,8 +33,25 @@ public class Event {
         this.description = description;
     }
 
+    public int getId() {
+        return id;
+    }
+
     @Override
     public String toString() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Event)) return false;
+        Event event = (Event) o;
+        return getId() == event.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
     }
 }
